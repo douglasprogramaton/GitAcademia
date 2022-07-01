05dc647bcf3ef214441d8669637058cd774ea5ea
douglasprogramaton <douglassantosprogramaton@gmail.com>
2022-07-01 11:52:29 -0300

    Serealização e Dessereialização

---
+++ SerealizacaoeDescerealizadacao/16_WinFormsAppSerializadorSOAP/Arquivo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _16_WinFormsAppSerializadorSOAP
+{
+    public class Arquivo
+    {
+        public static string lerArquivo(string dadosArquivo)
+        {
+            using (var leitor = new StreamReader(dadosArquivo))
+            {
+                return leitor.ReadToEnd();
+            }   
+        }
+    }
+}
